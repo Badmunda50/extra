ebf8aa57cfeacbaba223f10215485084a243d4d1
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-25 15:12:04 +0530

    Update blackword.py

--- plugins/blackword.py
+++ plugins/blackword.py
@@ -206,7 +206,7 @@ async def rm_allbl_callback(_, q: CallbackQuery):
     return
 
 
-@app.on_chat_member_updated()
+@app.on_message(filters.text & ~filters.edited)
 @adminsOnly("can_restrict_members")
 async def check_blacklisted_words(_, m: Message):
     db = Blacklist(m.chat.id)
